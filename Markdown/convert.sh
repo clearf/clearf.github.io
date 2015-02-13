dd96e799fa538b93bddcb29586769b434962a2c5
Clearfield Christopher <chris.clearfield@gmail.com>
2015-02-13 13:23:12 -0800

    Pulling conversion directly from local copy of Google Drive

--- Markdown/convert.sh
+++ Markdown/convert.sh
@@ -4,38 +4,45 @@ SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
 JEKYLL_ROOT="/Users/clearf/Documents/repos/system-logic-jekyll"
+MD_SOURCE=/Users/clearf/Google\ Drive/System-Logic\ \(shared\)/Website/published/Markdown/
 
-# Sync files from google drive
-cp -p ~/Google\ Drive/System-Logic\ \(shared\)/Website/published/Markdown/*.md ${JEKYLL_ROOT}/Markdown
-cp -prf ~/Google\ Drive/System-Logic\ \(shared\)/Website/published/Markdown/*_images ${JEKYLL_ROOT}/assets/images/
+# Sync image files from google drive
+cp -rf /Users/clearf/Google\ Drive/System-Logic\ \(shared\)/Website/published/Markdown/*_images ${JEKYLL_ROOT}/assets/images/
 
+curdir="`pwd`"
+
+cd ${MD_SOURCE}
 for file in *.md; do
+  # Does this file exist?
   if [ ! -f "$file" ]; then
     continue; 
   fi
-  header=$(head -n 1 "$file")
 
+  # Get header information
+  header=$(head -n 1 "$file")
   # If there is no header, then continue
   if [ `echo "$header" | grep -v "\|"` ]; then 
     continue; 
   fi
-
   title=$( echo $header | awk -F '|' '{print $1}' | awk -F ':' '{print $1}')
   subtitle=$( echo $header | awk -F '|' '{print $1}' | awk -F ':' '{print $2}' | sed 's/^ *//')
   date=$( echo $header | awk -F '|' '{print $2}' | awk -F ' ' '{print $1}')
   author=$( echo $header | awk -F '|' '{print $3}')
   tags=$( echo $header | awk -F '|' '{print $4}' | sed 's/, */,/g')
 
+  # Modfy the filename for postiing
   newfile="$date-$file"
 
-  mv "$file" "$newfile"
-  file="$newfile"
   postfile=${JEKYLL_ROOT}/_posts/commentary/$newfile 
-  if [ ! $newfile -nt $postfile ]; then  # || [! -f $postfile ]; then 
+
+  # If the post file is newer then we assume that edits have been made in place
+  # and don't update, but print a warning. 
+  if [ $postfile -nt $file ]; then  # || [! -f $postfile ]; then 
+    echo "Not updating `basename "$postfile"`. Newer than source file."
     continue;
   fi
 
-sed -i '' -e 1d \
+sed -e 1d \
 -e '2i\
 ---' \
 -e '2i \
@@ -49,15 +56,15 @@ tags: '"[$tags] " \
 -e '2i \
 permalink: ' \
 -e '2i \
----' "$file"
+---' "$file" > "$postfile"
 
   if [ ! -z "$subtitle" ]; then 
     sed -i '' -e '3i \
-    subtitle: '"\"$subtitle\"" "$newfile"
+    subtitle: '"\"$subtitle\"" "$postfile"
   fi
 
   echo "Posting $newfile"
-  cp $newfile $postfile
 done
 
 IFS=$SAVEIFS
+cd "$curdir"
